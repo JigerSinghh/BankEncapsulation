e0a7ad31e8948664ddb5fa9ce59273b0b11ee430
Jiger Dhillon <jigersingh29@gmail.com>
2023-05-24 20:38:55 -0400

    Finished

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var jigersAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            jigersAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank You! Your balance is now {jigersAccount.GetBalance()}");
+
         }
     }
 }
